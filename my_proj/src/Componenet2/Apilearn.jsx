c255d30c0df7aee26eea1036f3f59ea006d1ace3
Sintu4613S <shiva49767@gmail.com>
2026-07-03 20:12:30 +0530

    Take an Api and fetch it

--- my_proj/src/Componenet2/Apilearn.jsx
+++ my_proj/src/Componenet2/Apilearn.jsx
@@ -1,5 +1,9 @@
 
 const Apilearn = () => {
+  const API = 'https://jsonplaceholder.typicode.com/users'
+
+  let response = fetch(API)
+  console.log(response)
   return (
     <>
       <h1>Hello</h1>
