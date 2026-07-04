19d54fcda3e81fdda08639bb08d1fde390efe782
Sintu4613S <shiva49767@gmail.com>
2026-07-04 11:56:23 +0530

    Comment the params and show the data on the UI

--- my_proj/src/Component/Axiosfetch.jsx
+++ my_proj/src/Component/Axiosfetch.jsx
@@ -1,18 +1,37 @@
+import { useState } from 'react'
 import axios from 'axios'
 
+
 const Postdata = () => {
-  const fetchData = () => {
-    const response = axios({
+  const [users, setUsers] = useState([])
+  const fetchData = async () => {
+    const response = await axios({
       url: 'https://6a489aaca033dcb98d64b0ff.mockapi.io/users',
     })
-    console.log(response)
+    const data = response.data
+    setUsers(data)
   }
+  console.log(users)
   return (
-    <button onClick={fetchData} className="btn">FetchData2</button>
+    <>
+      <button onClick={fetchData} className="btn">FetchData2</button>
+      {users.map((userDetails) => {
+        return (
+          <li key={userDetails.id} className='bg-gray-700 p-6 rounded-lg my-2 '>
+            <p>Name:{userDetails.name}</p>
+            <p>Age:{userDetails.Age}</p>
+          </li>
 
+
+        )
+      })}
+
+    </>
   )
 
 }
+
+
 const Axiosfetch = () => {
   const config = {
     // url: 'https://jsonplaceholder.typicode.com/photos'
@@ -21,10 +40,10 @@ const Axiosfetch = () => {
       Accept: 'application/json'
     },
     timeout: 1000,
-    params: {
-      name: 'sintu',
-      age: 21
-    }
+    // params: {
+    //   name: 'sintu',
+    //   age: 21
+    // }
   }
   const fetchData = async () => {
     const response = await axios(config)
