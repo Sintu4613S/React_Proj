b16cb86eb7c3d1021ca49341fa8e90b4b07e92cc
Sintu4613S <shiva49767@gmail.com>
2026-07-06 16:28:12 +0530

    writ the logic for Detete data

--- my_proj/src/Component/Axiosfetch.jsx
+++ my_proj/src/Component/Axiosfetch.jsx
@@ -51,11 +51,13 @@ const Postdata = () => {
       age: ""
     })
   }
-  const delData = async () => {
+  const delData = async (userDetails) => {
+    console.log('data is delete')
+    console.log("User id h:", userDetails.id)
     await axios({
       url: `https://6a489aaca033dcb98d64b0ff.mockapi.io/users/${userDetails.id}`,
       method: 'delete',
-      // data: userDetails
+      data: null
     })
     fetchData()
     // setUserdetails({
@@ -63,50 +65,41 @@ const Postdata = () => {
     //   age: ""
     // })
   }
-}
-// const deleteData = async (id) => {
-//   const response = await axios({
-//     url: 'https://6a489aaca033dcb98d64b0ff.mockapi.io/users',
-//     method: 'delete',
-//   })
-//   console.log(response.data)
+  return (
+    <>
+      <button onClick={fetchData} className="btn mx-2">FetchData2</button>
+      <br /><br />
+      {/* Form for Data take From the User */}
+      <label htmlFor="nm">Name:
+        <input type="text" id='nm' placeholder='Enter Your Name' value={userDetails.name} name='name' onChange={handleOnchange} className="text-white " />
+      </label><br /><br />
+      <label htmlFor="ag">Age:
+        <input type="number" id='ag' placeholder='Enter Your Age' value={userDetails.age} name='age' onChange={handleOnchange} className="text-white " />
+      </label>
+      <br /><br />
+      {/* //*****Save Data**** */}
+      {userDetails.editMode === true ? (
+        <button onClick={saveData} className="btn mx-2">SaveData</button>
+      ) : (
+        <button disabled={!userDetails.name?.trim()} onClick={postData} className="btn mx-2">PostData</button>
 
-// }
-//console.log(users)
-return (
-  <>
-    <button onClick={fetchData} className="btn mx-2">FetchData2</button>
-    <br /><br />
-    {/* Form for Data take From the User */}
-    <label htmlFor="nm">Name:
-      <input type="text" id='nm' placeholder='Enter Your Name' value={userDetails.name} name='name' onChange={handleOnchange} className="text-white " />
-    </label><br /><br />
-    <label htmlFor="ag">Age:
-      <input type="number" id='ag' placeholder='Enter Your Age' value={userDetails.age} name='age' onChange={handleOnchange} className="text-white " />
-    </label>
-    <br /><br />
-    {userDetails.editMode === true ? (
-      <button onClick={saveData} className="btn mx-2">SaveData</button>
-    ) : (
-      <button disabled={!userDetails.name?.trim()} onClick={postData} className="btn mx-2">PostData</button>
-
-    )}
-    {/* <button onClick={deleteData} className="btn mx-2">DeleteData</button> */}
-    {users.map((userDetails) => {
-      return (
-        <li key={userDetails.id} className='bg-gray-700 p-6 rounded-lg my-2 text-center '>
-          <p>Name:{userDetails.name}</p>
-          <p>Age:{userDetails.age}</p>
-          <button className='btn' onClick={() => setUserdetails({ ...userDetails, editMode: true })}>Edit</button>
-          <button className='btn' onClick={delData}>Delete</button>
-        </li>
+      )}
+      {/* <button onClick={deleteData} className="btn mx-2">DeleteData</button> */}
+      {users.map((userDetails) => {
+        return (
+          <li key={userDetails.id} className='bg-gray-700 p-6 rounded-lg my-2 text-center list-none '>
+            <p>Name:{userDetails.name}</p>
+            <p>Age:{userDetails.age}</p>
+            <button className='btn m-2' onClick={() => setUserdetails({ ...userDetails, editMode: true })}>Edit</button>
+            <button className='btn m-2' onClick={() => delData(userDetails)}>Delete</button>
+          </li>
 
 
-      )
-    })}
+        )
+      })}
 
-  </>
-)
+    </>
+  )
 
 }
 
