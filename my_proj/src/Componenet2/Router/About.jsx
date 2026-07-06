c8a4a8254aa718793a05afe07596be20062789a0
Sintu4613S <shiva49767@gmail.com>
2026-07-06 20:26:53 +0530

    Make the About page to Display on the UI

---
+++ my_proj/src/Componenet2/Router/About.jsx
@@ -0,0 +1,15 @@
+
+
+const About = () => {
+  return (
+    <>
+      <div className="page">
+        <h1>This is a About page</h1>
+        <button className="btn">ADD</button>
+
+      </div>
+    </>
+  )
+}
+
+export default About
