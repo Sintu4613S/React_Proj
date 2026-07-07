2d6ca05af94ed4f90b60a134c42c2cc9bdd634a4
Sintu4613S <shiva49767@gmail.com>
2026-07-07 20:50:43 +0530

    Refactor About component to improve formatting and ensure Outlet is rendered correctly

--- my_proj/src/Componenet2/Router/About.jsx
+++ my_proj/src/Componenet2/Router/About.jsx
@@ -1,13 +1,15 @@
-
+import { Outlet } from "react-router-dom"
 
 const About = () => {
   return (
     <>
+
       <div className="page">
         <h1>This is a About page</h1>
         <button className="btn">ADD</button>
-
       </div>
+      <Outlet />
+
     </>
   )
 }
