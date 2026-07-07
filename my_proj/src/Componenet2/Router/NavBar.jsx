be83246cbcac23c2a1e4e8355b52a5c7545056db
Sintu4613S <shiva49767@gmail.com>
2026-07-07 20:18:52 +0530

    Replace Link with NavLink in NavBar component for enhanced routing features

--- my_proj/src/Componenet2/Router/NavBar.jsx
+++ my_proj/src/Componenet2/Router/NavBar.jsx
@@ -1,19 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const BasicRouter = () => {
   return (
     <>
-      <div className="flex gap-10 items-center text-4xl mt-2">
+      {/* <div className="flex gap-10 items-center text-4xl mt-2">
         <Link to="/">Home</Link>
         <Link to="/blog">Blog</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact Us</Link>
-
+      </div> */}
+      {/* *** NavLink*** use becuase its give some Extra feature*/}
+      <div className="flex gap-10 items-center text-4xl mt-2">
+        <NavLink end to="/" className={({ isActive }) => isActive ? 'text-red-500' : ''} >Home</NavLink>
+        <NavLink to="/blog" className={({ isActive }) => isActive ? 'text-red-500' : ''} >Blog</NavLink>
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-red-500' : ''} >About</NavLink>
+        <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-red-500' : ''} >Contact Us</NavLink>
       </div>
 
     </>
 
   )
 }
-
 export default BasicRouter
