1c14ffc9e573179b9343aad03ca791c84824bd7e
Sintu4613S <shiva49767@gmail.com>
2026-07-09 19:26:53 +0530

    Create a Home.jsx

---
+++ my_proj/src/Componenet2/NewRouter/Home2.jsx
@@ -0,0 +1,12 @@
+
+const Home2 = () => {
+  return (
+    <>
+      <div className="w-96 h-96 bg-yellow-500 text-center">
+        <h1>This is a Home Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home2
