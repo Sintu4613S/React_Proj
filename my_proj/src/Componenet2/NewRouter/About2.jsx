6c571386f4b17f131382456d895ad532f8cddb95
Sintu4613S <shiva49767@gmail.com>
2026-07-09 20:12:45 +0530

    Add data fetching with useLoaderData in About2 component

--- my_proj/src/Componenet2/NewRouter/About2.jsx
+++ my_proj/src/Componenet2/NewRouter/About2.jsx
@@ -1,5 +1,8 @@
+import { useLoaderData } from "react-router-dom"
 
 const About2 = () => {
+  const data = useLoaderData()
+  console.log(data)
   return (
     <>
       <div className="w-96 h-96 bg-yellow-500 mt-6 text-center text-3xl">
