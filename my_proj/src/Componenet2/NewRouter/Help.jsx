ef61639cd9591c77c754b9b2da4248c01cce7418
Sintu4613S <shiva49767@gmail.com>
2026-07-09 19:28:06 +0530

    Add the style

--- my_proj/src/Componenet2/NewRouter/Help.jsx
+++ my_proj/src/Componenet2/NewRouter/Help.jsx
@@ -3,7 +3,7 @@
 const Help = () => {
   return (
     <>
-      <div className="w-400 h-400 bg-yellow-500 text-center">
+      <div className="w-96 h-96 bg-yellow-500 text-center">
         <h1>This is a Help Page</h1>
       </div>
     </>
