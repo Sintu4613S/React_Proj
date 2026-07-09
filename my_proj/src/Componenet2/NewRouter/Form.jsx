36cfcf795dcb89ad2b5c49e080eb74c0e02087e8
Sintu4613S <shiva49767@gmail.com>
2026-07-09 19:57:47 +0530

    Create a For Commponet that render in the help component

---
+++ my_proj/src/Componenet2/NewRouter/Form.jsx
@@ -0,0 +1,14 @@
+
+
+const Form = () => {
+  return (
+    <>
+      <div className=" mt-5 text-center text-2xl">
+        <h1>This is A Form.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sit blanditiis commodi rem nihil porro! Neque expedita mollitia, quis eligendi beatae illo, sit quia dicta perspiciatis sunt suscipit aliquid accusamus! Id doloribus nobis ratione?</p>
+      </div>
+
+    </>)
+}
+
+export default Form
