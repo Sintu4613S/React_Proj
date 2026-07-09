8d619038d6cb4a1a9998595d48d8c23bfba9eca1
Sintu4613S <shiva49767@gmail.com>
2026-07-09 19:26:23 +0530

    Create a Help.jsx

---
+++ my_proj/src/Componenet2/NewRouter/Help.jsx
@@ -0,0 +1,13 @@
+
+
+const Help = () => {
+  return (
+    <>
+      <div className="w-400 h-400 bg-yellow-500 text-center">
+        <h1>This is a Help Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default Help
