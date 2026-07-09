1fa4212e66716b9b9fbb6053d8c363b675ac404e
Sintu4613S <shiva49767@gmail.com>
2026-07-09 19:26:05 +0530

    Create A About.jsx

---
+++ my_proj/src/Componenet2/NewRouter/About2.jsx
@@ -0,0 +1,12 @@
+
+const About2 = () => {
+  return (
+    <>
+      <div className="w-96 h-96 bg-yellow-500 text-center">
+        <h1>This is a About Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default About2
