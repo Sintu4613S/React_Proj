e88d9abe504dac3edb6da13f7ce8d765f72e11fb
Sintu4613S <shiva49767@gmail.com>
2026-07-11 17:45:01 +0530

    Set up React Router with Home and Products routes

--- Proj_01/src/main.jsx
+++ Proj_01/src/main.jsx
@@ -1,9 +1,30 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Products from './Component/Products.jsx'
+import Home from './Component/Home.jsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/products',
+        element: <Products />
+      }
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
+  <>
+    <RouterProvider router={router} />
 
-  <App />
-
+  </>
 )
