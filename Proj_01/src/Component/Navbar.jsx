7c158a408a49883fb6afa5c59479269f9c63c714
Sintu4613S <shiva49767@gmail.com>
2026-07-12 20:33:00 +0530

    Use the Navlink instaed  of the Link and use the Menu component  for everyLink using with the props

--- Proj_01/src/Component/Navbar.jsx
+++ Proj_01/src/Component/Navbar.jsx
@@ -1,14 +1,20 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import storelogo from '../assets/store.png'
 const Navbar = () => {
   return (
     <>
       <div>
-        <nav className='tw:flex tw:gap-4 tw:bg-[#eee]'>
+        <nav className='tw:flex tw:gap-4 tw:bg-[#eee] tw:items-center'>
           <img src={storelogo} className='tw:w-12 tw:mr-3' alt=" store_image" />
-          <Link to="/" className='nav-link' >Home</Link>
-          <Link to="/products" className='nav-link'>Products</Link>
-          <Link to="/about" className='nav-link'>About</Link>
+
+          {/* <NavLink to="/" className='nav-link' >Home</NavLink>
+          <NavLink to="/products" className='nav-link'>Products</NavLink>
+          <NavLink to="/about" className='nav-link'>About</NavLink> */}
+          <div className=' tw:flex tw:flex-row tw:gap-7'>
+            <Menu to={'/'} title={'Home'} />
+            <Menu to={'/products'} title={'Products'} />
+            <Menu to={'/about'} title={'About'} />
+          </div>
         </nav>
       </div>
     </>
@@ -16,3 +22,10 @@ const Navbar = () => {
 }
 
 export default Navbar
+const Menu = ({ to, title }) => {
+  return (
+    <NavLink to={to} className={({ isActive }) => `nav-link ${isActive ? 'tw:!text-[#ff6347]' : ''}`}>
+      {title}
+    </NavLink>
+  )
+}
