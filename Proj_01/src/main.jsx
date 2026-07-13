f68d31c4e78321d97f44106a7affbbac8ccce8f2
Sintu4613S <shiva49767@gmail.com>
2026-07-13 23:03:14 +0530

    Refactor main.jsx to remove unused router setup and integrate Mycontext provider

--- Proj_01/src/main.jsx
+++ Proj_01/src/main.jsx
@@ -1,41 +1,47 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Products from './Component/Products.jsx'
-import Home from './Component/Home.jsx'
-import About from './Component/About.jsx'
-import SingleProduct from './Component/SingleProduct.jsx'
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/products',
-        element: <Products />
-      },
-      {
-        path: '/products/:id',
-        element: <SingleProduct />
-      },
-      {
-        path: '/about',
-        element: <About />
-      }
+import { Mycontext } from './Component/StateManage/Mycontext.jsx'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Products from './Component/Products.jsx'
+// import Home from './Component/Home.jsx'
+// import About from './Component/About.jsx'
+// import SingleProduct from './Component/SingleProduct.jsx'
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />
+//       },
+//       {
+//         path: '/products',
+//         element: <Products />
+//       },
+//       {
+//         path: '/products/:id',
+//         element: <SingleProduct />
+//       },
+//       {
+//         path: '/about',
+//         element: <About />
+//       }
+//     ]
+//   }
+// ])
 
+// createRoot(document.getElementById('root')).render(
+//   <>
+//     <RouterProvider router={router} />
 
-    ]
-  }
-])
-
+//   </>
+// )
 createRoot(document.getElementById('root')).render(
   <>
-    <RouterProvider router={router} />
-
+    <Mycontext.Provider value={"SintuKumar"}>
+      <App />
+    </Mycontext.Provider>
   </>
 )
