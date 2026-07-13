2ea27b272fea2e1c099f866d856a95934b0cf5ca
Sintu4613S <shiva49767@gmail.com>
2026-07-13 23:25:10 +0530

    Add the functionality of usecontext in the context Api taht help to instead of importing the  the context and MyContext, Now Simply import only UseMyContext custom hooks

--- Proj_01/src/Component/StateManage/Mycontext.jsx
+++ Proj_01/src/Component/StateManage/Mycontext.jsx
@@ -1,4 +1,11 @@
 
-import { createContext } from "react"
-export const Mycontext = createContext();
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext } from "react"
+const Mycontext = createContext();
+
+// Use the Context
+const useMyContext = () => {
+  return useContext(Mycontext)
+}
+export { Mycontext, useMyContext }
 
