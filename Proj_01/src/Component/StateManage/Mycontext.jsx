302ac7ea8415945d070dcb1b43983eaf9c3d3c6e
Sintu4613S <shiva49767@gmail.com>
2026-07-13 23:49:53 +0530

    Add the provider  here and use  here for more refactor by binding it in Component

--- Proj_01/src/Component/StateManage/Mycontext.jsx
+++ Proj_01/src/Component/StateManage/Mycontext.jsx
@@ -1,11 +1,22 @@
-
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext } from "react"
+//Create th Context
 const Mycontext = createContext();
+
+//Provider
+const MyContextProvider = ({ children }) => {
+  return (
+    <Mycontext.Provider value={"SintuKumar"}>
+      {children}
+    </Mycontext.Provider>
+  )
+}
 
 // Use the Context
 const useMyContext = () => {
   return useContext(Mycontext)
 }
-export { Mycontext, useMyContext }
+
+//export { Mycontext, useMyContext }
+export { MyContextProvider, useMyContext }
 
