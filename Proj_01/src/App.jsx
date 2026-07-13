ef50fc5004637d5d669fc3c0fb7798dbf95d6d3b
Sintu4613S <shiva49767@gmail.com>
2026-07-13 23:26:20 +0530

    remove the Context Api from the App because not using it

--- Proj_01/src/App.jsx
+++ Proj_01/src/App.jsx
@@ -1,19 +1,17 @@
-import { useContext } from 'react'
 import './App.css'
 // import Navbar from './Component/Navbar'
 // import { Outlet } from 'react-router-dom'
 import A from './Component/StateManage/A'
-import { Mycontext } from './Component/StateManage/Mycontext'
+
 
 function App() {
-  const data = useContext(Mycontext)
-  console.log(data)
+
   return (
     <>
       {/* <Navbar />
       <Outlet /> */}
       <div className='tw:bg-fuchsia-600 tw:p-10 tw:text-center'>
-        App
+        <h1>App</h1>
         <A />
       </div>
     </>
