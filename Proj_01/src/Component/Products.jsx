0f7aacd4a2b30bff412c8e2a0dcebc6278d9e793
Sintu4613S <shiva49767@gmail.com>
2026-07-13 20:33:12 +0530

    Enhance ProductCard component with navigation and hover effects

--- Proj_01/src/Component/Products.jsx
+++ Proj_01/src/Component/Products.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Loader from './Loader'
+import { useNavigate } from 'react-router-dom'
 const API = 'https://fakestoreapi.com/products'
 const Products = () => {
   const [products, setProducts] = useState([])
@@ -36,10 +37,11 @@ const Products = () => {
 }
 
 const ProductCard = ({ items }) => {
+  const navigate = useNavigate()
   return (
     <>
-      <div className='tw:bg-gray-300 tw:rounded-lg'>
-        <img src={items.image} className='tw:aspect-square tw:object-contain tw:p-4' alt="product_img" />
+      <div onClick={() => navigate(`/products/${items.id}`)} className='tw:bg-gray-300 tw:rounded-lg tw:group'>
+        <img src={items.image} className='tw:aspect-square tw:object-contain tw:p-4 tw:group-hover:scale-90 tw:transition-all tw:duration-400' alt="product_img" />
         <div className='tw:p-5 '>
           <div className="tw:p-2">
             <span className='tw:text-xsm tw:bg-green-500 tw:w-fit tw:rounded-lg'>
@@ -48,7 +50,7 @@ const ProductCard = ({ items }) => {
             |
             <span>{items.rating['count']}</span>
           </div>
-          <h5 className='tw:line-clamp-2'> {items.title}</h5>
+          <h5 className='tw:line-clamp-2 tw:group-hover:text-blue-400'> {items.title}</h5>
           <p className='tw:text-xl tw:font-medium'>{items.price}Rs</p>
         </div>
       </div>
