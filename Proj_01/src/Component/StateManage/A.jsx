b414967564bb5ec07c88e336a547c51a45f96fff
Sintu4613S <shiva49767@gmail.com>
2026-07-13 23:29:34 +0530

    Commented code is used to import the Context and Mycontext and other is  used that  wraps the context and MyContext in a Single Custm hooks in the MyContext.

---
+++ Proj_01/src/Component/StateManage/A.jsx
@@ -0,0 +1,18 @@
+import B from "./B"
+// import { useContext } from "react"
+// import { Mycontext } from "./Mycontext"
+import { useMyContext } from './Mycontext'
+
+const A = () => {
+  // const data = useContext(Mycontext)
+  const data = useMyContext();
+  return (
+    <div className="tw:bg-amber-600 tw:p-10">
+      <h1>Hello From A  Component</h1>
+      <p className="tw:bg-amber-200">My Name is{data}</p>
+      <B />
+    </div>
+  )
+}
+
+export default A
