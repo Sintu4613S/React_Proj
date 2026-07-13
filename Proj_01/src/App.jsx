d7a2f966603bcc4e035e407e418fc1c9f2b03403
Sintu4613S <shiva49767@gmail.com>
2026-07-13 23:02:53 +0530

    Implement context management and add new components for state management

--- Proj_01/src/App.jsx
+++ Proj_01/src/App.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react'
 import './App.css'
-import Navbar from './Component/Navbar'
-import { Outlet } from 'react-router-dom'
+// import Navbar from './Component/Navbar'
+// import { Outlet } from 'react-router-dom'
+import A from './Component/StateManage/A'
+import { Mycontext } from './Component/StateManage/Mycontext'
 
 function App() {
+  const data = useContext(Mycontext)
+  console.log(data)
   return (
     <>
-      <Navbar />
-      <Outlet />
+      {/* <Navbar />
+      <Outlet /> */}
+      <div className='tw:bg-fuchsia-600 tw:p-10 tw:text-center'>
+        App
+        <A />
+      </div>
     </>
   )
 }
