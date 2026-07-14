7ff70760d840525b822904036c9d8e90f539a282
Sintu4613S <shiva49767@gmail.com>
2026-07-14 12:07:39 +0530

    Add About component with Loader

---
+++ Proj_01/src/Component/About.jsx
@@ -0,0 +1,16 @@
+import Loader from "./Loader"
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>This is a About page....</h1>
+        <Loader />
+      </div>
+
+    </>
+
+  )
+}
+
+export default About
