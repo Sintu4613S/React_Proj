85b339776e6896881cf3b3d42750d9581cb87d11
Sintu4613S <shiva49767@gmail.com>
2026-07-14 09:49:05 +0530

    use ths state that is defined in the Mycontext and  update the State in D component and reflect in A Component

--- Proj_01/src/Component/StateManage/A.jsx
+++ Proj_01/src/Component/StateManage/A.jsx
@@ -4,12 +4,14 @@ import B from "./B"
 import { useMyContext } from './Mycontext'
 
 const A = () => {
-  // const data = useContext(Mycontext)
-  // const data = useMyContext();
+  //1. const data = useContext(Mycontext)
+  //2. const data = useMyContext();
+  const { count } = useMyContext();
   // console.log(data)
   return (
     <div className="tw:bg-amber-600 tw:p-10">
       <h1>Hello From A  Component</h1>
+      <h4>{count}</h4>
       {/* <p className="tw:bg-amber-200">My Name is{data}</p> */}
       <B />
     </div>
