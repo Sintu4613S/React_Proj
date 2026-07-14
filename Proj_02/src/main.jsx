f8f0290bf33ea395533eee58d870e994b3354b3f
Sintu4613S <shiva49767@gmail.com>
2026-07-14 20:32:51 +0530

    Provide the NewsContexprovider for  make a global sttore and acces the data in all component

--- Proj_02/src/main.jsx
+++ Proj_02/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { NewsContextProvider } from './components/pages/context/NewsContex.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <NewsContextProvider>
     <App />
-  </StrictMode>,
+  </NewsContextProvider>
 )
