2d4ae997c5ba1bb1940ee0754edc09852e0b88e0
Sintu4613S <shiva49767@gmail.com>
2026-07-14 09:49:43 +0530

    Mange the Count State that reflects over the Ui

--- Proj_01/src/Component/StateManage/D.jsx
+++ Proj_01/src/Component/StateManage/D.jsx
@@ -2,12 +2,13 @@
 // import { Mycontext } from "./Mycontext"
 import { useMyContext } from './Mycontext'
 const D = () => {
-  // const data = useContext(Mycontext)
-  // const data = useMyContext();
+  //1. const data = useContext(Mycontext)
+  //2. const data = useMyContext();
+  const { setCount } = useMyContext();
   return (
     <div className="tw:bg-blue-600 tw:p-4">
       <h1>Hello From D  Component</h1>
-      <button>Count</button>
+      <button onClick={() => setCount(prev => prev + 1)}>Count</button>
       {/* <p className="tw:bg-blue-200">My Name is{data}</p> */}
     </div>
   )
