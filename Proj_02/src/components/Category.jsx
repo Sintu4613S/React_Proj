a5097304beac0b4811193f781d247e303b0c10f1
Sintu4613S <shiva49767@gmail.com>
2026-07-14 14:47:35 +0530

    Create a Button depend on the Api Category

---
+++ Proj_02/src/components/Category.jsx
@@ -0,0 +1,21 @@
+import Wrapper from "./Wrapper"
+
+
+const Category = ({ className }) => {
+  const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+  return (
+    <Wrapper>
+      <div className={`max-w-full w-fit flex m-auto overflow-x-auto p-1.5 scrollbar-none gap-3 ${className} `} >
+        {categories.map((category) => {
+          return (
+            <button key={category} className="btn btn-primary">{category.charAt(0).toUpperCase() + category.slice(1)}</button>
+          )
+        })}
+      </div>
+
+    </Wrapper>
+
+  )
+}
+
+export default Category
