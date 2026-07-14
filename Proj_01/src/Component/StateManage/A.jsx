27e9a6160bc4de3f6985a5cec613fd0f68df4508
Sintu4613S <shiva49767@gmail.com>
2026-07-14 09:30:15 +0530

    comment the code and use the state

--- Proj_01/src/Component/StateManage/A.jsx
+++ Proj_01/src/Component/StateManage/A.jsx
@@ -5,11 +5,12 @@ import { useMyContext } from './Mycontext'
 
 const A = () => {
   // const data = useContext(Mycontext)
-  const data = useMyContext();
+  // const data = useMyContext();
+  // console.log(data)
   return (
     <div className="tw:bg-amber-600 tw:p-10">
       <h1>Hello From A  Component</h1>
-      <p className="tw:bg-amber-200">My Name is{data}</p>
+      {/* <p className="tw:bg-amber-200">My Name is{data}</p> */}
       <B />
     </div>
   )
