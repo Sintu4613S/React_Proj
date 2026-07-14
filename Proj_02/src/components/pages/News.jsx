0dc3f0ba55c2d72fbe373cc81971a39760fb677f
Sintu4613S <shiva49767@gmail.com>
2026-07-14 20:35:11 +0530

    Render the News through custom useNewContext component and displa them using map and tailwindcss css to show  in well manner

--- Proj_02/src/components/pages/News.jsx
+++ Proj_02/src/components/pages/News.jsx
@@ -1,34 +1,50 @@
+import { useEffect } from "react"
 import Wrapper from "../Wrapper"
+import { useNewsContext } from "./context/NewsContex"
 
 
 const News = ({ className }) => {
+  const { news, setNews, fetchNews } = useNewsContext()
+  console.log(news)
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchNews()
+      //console.log(data)
+      setNews(data.articles)
+    })()
+  }, [])
+
   return (
     <Wrapper>
-      <div className={`grid grid-cols-4 gap-6  ${className}`}>
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
+      <div className={`max-w[1200px] grid grid-cols-1 sm:grid-cols-2
+        md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6  ${className}`}>
+        {news.map((newsDetails, index) => {
+          return (
+            <Newscard key={index} details={newsDetails} />
+          )
+          //console.log(newsDetails)
+        })}
 
       </div>
     </Wrapper>
   )
 }
-const Newscard = () => {
+const Newscard = ({ details }) => {
   return (
     <Wrapper>
       <div className={`card bg-base-200 shadow-sm`}>
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src={details?.urlToImage}
             alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <span className="text-xl text-green-600">{details.publishedAt}</span>
+          <h2 className="card-title line-clamp-2 text-2xl">{details?.title}...</h2>
+          <p className="line-clamp-2 text-xl">{details.description}...</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Read More</button>
+            <button onClick={() => window.open(details.url, "_blank")} className="btn badge-outline my-2">Read More</button>
           </div>
         </div>
       </div>
