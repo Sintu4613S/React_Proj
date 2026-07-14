efe8c44f73de408cdb6434ece1038291b1d312b7
Sintu4613S <shiva49767@gmail.com>
2026-07-14 23:05:39 +0530

    handle the category  news means calls the news related to category

--- Proj_02/src/components/Category.jsx
+++ Proj_02/src/components/Category.jsx
@@ -1,17 +1,27 @@
+import { useNewsContext } from "./pages/context/NewsContex"
 import Wrapper from "./Wrapper"
-
 
 const Category = ({ className }) => {
   const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+  const { setNews, fetchNews } = useNewsContext()
+
+  const handleCategory = async (e) => {
+    const catgy = e.target.value
+    //console.log(catgy)
+    const data = await fetchNews(`/everything?q=${catgy}`)
+    //console.log(data)
+    setNews(data.articles)
+  }
   return (
     <div className={`${className}`}>
-
-
       <Wrapper>
         <div className={`max-w-full w-fit flex m-auto overflow-x-auto p-1.5 scrollbar-none gap-3  `} >
           {categories.map((category) => {
             return (
-              <button key={category} className="btn btn-primary">{category.charAt(0).toUpperCase() + category.slice(1)}</button>
+              <button onClick={handleCategory}
+                key={category}
+                value={category}
+                className="btn btn-primary">{category.charAt(0).toUpperCase() + category.slice(1)}</button>
             )
           })}
         </div>
