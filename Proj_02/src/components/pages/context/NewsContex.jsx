7e3cab081d8bb9a534a74d7a096c85606682c348
Sintu4613S <shiva49767@gmail.com>
2026-07-14 20:31:36 +0530

    Create a cotext,provide context to thm app in main.jsx and use the context value through all the component

---
+++ Proj_02/src/components/pages/context/NewsContex.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from "react";
+import axios from 'axios'
+
+const NewsContext = createContext();
+
+const NewsContextProvider = ({ children }) => {
+  const [news, setNews] = useState([])
+
+  const fetchNews = async (url = '/everything?q=india') => {
+    try {
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const response = await axios(`${apiUrl}${url}&apiKey=${apiKey}`)
+
+      return response.data
+
+    } catch (error) {
+      console.log(error)
+
+    }
+
+  }
+  const value = {
+    news,
+    setNews,
+    fetchNews
+  }
+  return (
+    <NewsContext.Provider value={value}>
+      {children}
+    </NewsContext.Provider>
+  )
+}
+const useNewsContext = () => {
+  return useContext(NewsContext)
+}
+
+export { NewsContextProvider, useNewsContext }
