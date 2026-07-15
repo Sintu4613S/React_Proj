8c2e9e113eb0efc6c5f5469fc279e4cdd7c08b84
Sintu4613S <shiva49767@gmail.com>
2026-07-15 16:04:46 +0530

    isme rightbar ko import kiya aurek svg lagaya aur uspe onclick lagaya taki click p isopen state of righbar ki true ho jae aur righbar componenet ko render kr ske

--- Proj_02/src/components/Navbar.jsx
+++ Proj_02/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
 import Wrapper from "./Wrapper"
 import { useNewsContext } from "./pages/context/NewsContex"
+import Rightbar from "./pages/context/Rightbar"
 
 const Navbar = ({ className }) => {
   const [searchValue, setSearchValue] = useState('')
-  const { setNews, fetchNews } = useNewsContext()
+  const { setNews, fetchNews, setIsopen } = useNewsContext()
   // const onChange = (e) => {
   //   const value = e.target.value
   //   setSearchValue(value)
   // }
   //console.log(searchValue)
+
   useEffect(() => {
     if (!searchValue.trim()) return
     const delayDebounce = setTimeout(() => {
@@ -28,6 +30,7 @@ const Navbar = ({ className }) => {
     }, 1000)
     return () => clearTimeout(delayDebounce)
   }, [searchValue])
+
   return (
     <div className={`bg-base-300  ${className}`}>
       <Wrapper>
@@ -37,16 +40,21 @@ const Navbar = ({ className }) => {
           </div>
           <div className="flex gap-2">
             <input type="text" onChange={(e) => setSearchValue(e.target.value)} placeholder="Search" value={searchValue} className="input input-bordered w-24 md:w-auto" />
-            <button className="btn btn-ghost btn-circle">
+            <button onClick={() => setIsopen(true)} className="btn btn-ghost btn-circle cursor-pointer">
               <div className="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-                <span className="badge badge-xs badge-primary indicator-item"></span>
+                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path fill="currentColor" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z" />
+                  <path fill="#fff" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z" />
+                </svg>
               </div>
             </button>
           </div>
+
         </div>
-      </Wrapper>
-    </div>
+        <Rightbar />
+      </Wrapper >
+    </div >
+
   )
 }
 
