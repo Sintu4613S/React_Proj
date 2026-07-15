48fc80b3a9c9f6db20ecded6890cc18d0d1e46cd
Sintu4613S <shiva49767@gmail.com>
2026-07-15 11:35:37 +0530

    comment the func and add it as a inline fn

--- Proj_02/src/components/Navbar.jsx
+++ Proj_02/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { useNewsContext } from "./pages/context/NewsContex"
 const Navbar = ({ className }) => {
   const [searchValue, setSearchValue] = useState('')
   const { setNews, fetchNews } = useNewsContext()
-  const onChange = (e) => {
-    const value = e.target.value
-    setSearchValue(value)
-  }
+  // const onChange = (e) => {
+  //   const value = e.target.value
+  //   setSearchValue(value)
+  // }
   //console.log(searchValue)
   useEffect(() => {
     if (!searchValue.trim()) return
@@ -36,7 +36,7 @@ const Navbar = ({ className }) => {
             <a className="btn btn-ghost text-xl">DailyNews</a>
           </div>
           <div className="flex gap-2">
-            <input type="text" onChange={onChange} placeholder="Search" value={searchValue} className="input input-bordered w-24 md:w-auto" />
+            <input type="text" onChange={(e) => setSearchValue(e.target.value)} placeholder="Search" value={searchValue} className="input input-bordered w-24 md:w-auto" />
             <button className="btn btn-ghost btn-circle">
               <div className="indicator">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
