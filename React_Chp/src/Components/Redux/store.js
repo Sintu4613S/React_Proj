da2ed7880ac68d62b87cef5caebaf9fcd3089a92
Sintu4613S <shiva49767@gmail.com>
2026-07-18 09:39:33 +0530

    Refactor store setup to combine product and cart reducers

--- React_Chp/src/Components/Redux/store.js
+++ React_Chp/src/Components/Redux/store.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 import { productReducer } from './Reducer/productreducer'
-let store = createStore(productReducer);
+import { cartReducer } from './Reducer/cartReducer';
+//Combine the Two Reducre or more than two
+const rootReducer = combineReducers({
+  productReducer,
+  cartReducer
+})
+let store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+);
 
 
 export default store;
