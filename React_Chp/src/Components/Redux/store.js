69c4a9853ec067a78d91b0218b3bbd6421093b44
Sintu4613S <shiva49767@gmail.com>
2026-07-18 09:48:03 +0530

    Rfactor the store.js remove the reducer from it make a differnt file and store it and imort that file in the store.js

--- React_Chp/src/Components/Redux/store.js
+++ React_Chp/src/Components/Redux/store.js
@@ -1,11 +1,7 @@
-import { combineReducers, createStore } from 'redux'
-import { productReducer } from './Reducer/productreducer'
-import { cartReducer } from './Reducer/cartReducer';
+import { createStore } from 'redux'
+import rootReducer from './Reducer';
 //Combine the Two Reducre or more than two
-const rootReducer = combineReducers({
-  productReducer,
-  cartReducer
-})
+
 let store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
