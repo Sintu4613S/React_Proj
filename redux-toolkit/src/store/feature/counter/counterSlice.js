b74d36ee1ff8881e437df341645690784b759247
Sintu4613S <shiva49767@gmail.com>
2026-07-18 14:21:31 +0530

    feat: add counter slice with increment, decrement, and changeByValue reducers

---
+++ redux-toolkit/src/store/feature/counter/counterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0,
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.value += 1
+    }
+  },
+  decrement: (state, action) => {
+    state.value -= 1
+  },
+  changeByValue: (state, action) => {
+    state.value = action.payload
+  }
+})
+export { increment, decrement, changeByValue } = counterSlice.actions
+export default counterSlice.reducer
+
+
