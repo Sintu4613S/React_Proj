b45481c4c136537955bb594edc5f53cfdef422a2
Sintu4613S <shiva49767@gmail.com>
2026-07-18 13:07:55 +0530

    remove unused productReducer

--- React_Chp/src/Components/Redux/Reducer/Productpage/Product.jsx
+++ React_Chp/src/Components/Redux/Reducer/Productpage/Product.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { productReducer } from '../productreducer'
-
 const Product = () => {
   const products = useSelector((state) => state?.productReducer?.products ?? [])
 
