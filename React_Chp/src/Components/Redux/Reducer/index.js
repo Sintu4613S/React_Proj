d1fbac9857073a61af8a5418f3d8bc292d032042
Sintu4613S <shiva49767@gmail.com>
2026-07-18 13:06:47 +0530

    refactor: remove cartReducer import from rootReducer

--- React_Chp/src/Components/Redux/Reducer/index.js
+++ React_Chp/src/Components/Redux/Reducer/index.js
@@ -1,8 +1,9 @@
 //Main root for the Reducer
 import { combineReducers } from 'redux'
-import { productReducer } from './Reducer/productreducer'
-import { cartReducer } from './Reducer/cartReducer'; const rootReducer = combineReducers({
+import { productReducer } from './productreducer'
+
+
+const rootReducer = combineReducers({
   productReducer,
-  cartReducer
 })
 export default rootReducer
