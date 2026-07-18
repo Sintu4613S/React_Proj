88d20888802792414bab972b11c7e0269c764d9b
Sintu4613S <shiva49767@gmail.com>
2026-07-18 09:39:50 +0530

    Add product and cart action creators

---
+++ React_Chp/src/Components/Redux/Action/productAction.js
@@ -0,0 +1,12 @@
+export const addtoProduct = (details) => {
+  return {
+    type: 'ADD_TO_PRODUCT',
+    payload: details,
+  }
+}
+export const addtoCart = (details) => {
+  return {
+    type: 'ADD_TO_CART',
+    payload: details,
+  }
+}
