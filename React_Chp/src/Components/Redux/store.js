1583b190d8f0eb9270fe5bd55629f16be844661e
Sintu4613S <shiva49767@gmail.com>
2026-07-18 10:08:16 +0530

    remove the middleware

--- React_Chp/src/Components/Redux/store.js
+++ React_Chp/src/Components/Redux/store.js
@@ -1,10 +1,10 @@
-import { applyMiddleware, createStore } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from './Reducer';
 //Combine the Two Reducre or more than two
 
 let store = createStore(
   rootReducer,
-  applyMiddleware(),
+
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 );
