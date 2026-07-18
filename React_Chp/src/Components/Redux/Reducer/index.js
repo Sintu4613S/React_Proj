852e83d2df9aa69797d0ef893c223e512e0bc58a
Sintu4613S <shiva49767@gmail.com>
2026-07-18 19:43:12 +0530

    Addthe cartReduer

--- React_Chp/src/Components/Redux/Reducer/index.js
+++ React_Chp/src/Components/Redux/Reducer/index.js
@@ -1,9 +1,11 @@
 //Main root for the Reducer
 import { combineReducers } from 'redux'
 import { productReducer } from './productreducer'
+import { cartReducer } from './cartReducer'
 
 
 const rootReducer = combineReducers({
   productReducer,
+  cartReducer
 })
 export default rootReducer
