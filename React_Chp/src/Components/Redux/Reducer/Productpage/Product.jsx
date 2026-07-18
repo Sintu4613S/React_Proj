ece67942d733176973180220119b5d6322e3b7fd
Sintu4613S <shiva49767@gmail.com>
2026-07-18 10:09:05 +0530

    nothing change

--- React_Chp/src/Components/Redux/Reducer/Productpage/Product.jsx
+++ React_Chp/src/Components/Redux/Reducer/Productpage/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { productReducer } from '../productreducer'
 
 const Product = () => {
   const products = useSelector((state) => state?.productReducer?.products ?? [])
