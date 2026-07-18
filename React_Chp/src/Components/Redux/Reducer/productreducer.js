051f376e0ac848dd1513cc3146d36da122fd63cc
Sintu4613S <shiva49767@gmail.com>
2026-07-18 09:40:51 +0530

    Fix product reducer to handle ADD_TO_PRODUCT action correctly

--- React_Chp/src/Components/Redux/Reducer/productreducer.js
+++ React_Chp/src/Components/Redux/Reducer/productreducer.js
@@ -1,13 +1,13 @@
 const initialState = {
   products: [],
 }
+
 export const productReducer = (state = initialState, action) => {
   if (action.type === 'ADD_TO_PRODUCT') {
-    console.log('add to product trigger hua h')
-    return state
-  }
-  else {
-    return state;
+    return {
+      products: [...state.products, action.payload],
+    }
   }
 
+  return state
 }
