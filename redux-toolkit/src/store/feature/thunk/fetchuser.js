a55382dc03c160823c4686d918865aaf2397aaca
Sintu4613S <shiva49767@gmail.com>
2026-07-18 22:11:59 +0530

    creat a seperate file for fetchin the data from Api keep it in thunk folder

---
+++ redux-toolkit/src/store/feature/thunk/fetchuser.js
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+export const fetchUser = createAsyncThunk(
+  'users/fetchUser',
+  async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      console.log(data)
+
+    } catch (error) {
+      console.log("Error Occured", error)
+
+    }
+  })
