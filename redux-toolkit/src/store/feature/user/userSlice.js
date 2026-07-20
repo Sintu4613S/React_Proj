06a2a008d90f8fc8b88f662aee4eda5f3b7bae43
Sintu4613S <shiva49767@gmail.com>
2026-07-20 13:27:56 +0530

    import the fetchuser and create extra reducers

--- redux-toolkit/src/store/feature/user/userSlice.js
+++ redux-toolkit/src/store/feature/user/userSlice.js
@@ -1,15 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUser } from "../thunk/fetchuser";
 
 const initialState = {
-  user: []
+
+  users: [],
+  status: 'idle',
+  error: null,
+  loading: false
+
 }
 
-
 const userSlice = createSlice({
-  name: 'users',
+  name: 'user',
   initialState,
   reducers: {
-  }
+  },
+  extraReducers: (Builder) => {
+    Builder.addCase(fetchUser.pending, (state) => {
+      state.status = "loading";
+      state.loading = true
+      state.error = null
+    })
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.loading = false
+        state.users = action.payload
+        //console.log(action.payload)
+      })
+      .addCase(fetchUser.rejected, (state, action) => {
+        console.log("Errorhai", action)
+        console.log("Exact Error:", action.error?.message)
+        state.status = "failed";
+        state.error = action.error;
+        state.loading = false;
+
+      });
+  },
 
 })
 
