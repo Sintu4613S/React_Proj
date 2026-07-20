28083708494a74af90020f2aff66ada7a207388c
Sintu4613S <shiva49767@gmail.com>
2026-07-20 20:43:32 +0530

    import the poslist

--- Proj_03/src/App.jsx
+++ Proj_03/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import PostList from './features/PostList'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-red-500 text-3xl'>Hello</h1>
+      {/* <h1 className='bg-red-500 text-3xl'>Hello</h1> */}
+      <PostList />
     </>
   )
 }
