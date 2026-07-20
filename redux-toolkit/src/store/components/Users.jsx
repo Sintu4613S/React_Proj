52b9acf9a4d8872ba154a23cee5af1c79b782f39
Sintu4613S <shiva49767@gmail.com>
2026-07-20 13:27:20 +0530

    dispactch the fetchUser and display the Users Details by mapping it

--- redux-toolkit/src/store/components/Users.jsx
+++ redux-toolkit/src/store/components/Users.jsx
@@ -1,16 +1,33 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { fetchUser } from "../feature/thunk/fetchuser"
 
 
 const Users = () => {
+  const users = useSelector(state => state.user.users)
+  console.log(users)
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(fetchUser())
-  })
+  }, [])
+
   return (
     <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id} style={{
+            width: '200px',
+            marginBottom: '8px',
+            overflowX: 'auto',
+            height: '150px', borderRadius: '10px', border: '2px solid gray', textAlign: 'center'
+          }}>
+            Name:{user.name}<br />
+            UserName: {user.username} <br />Email:{user.email}
+          </div>
 
+        )
+      })}
     </div>
   )
 }
