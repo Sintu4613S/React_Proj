1a4901c07d5cc25a34147d6744b7f0a73cc141d4
Sintu4613S <shiva49767@gmail.com>
2026-07-21 10:44:13 +0530

    fix import paths for PostList and ContentForm components in App.jsx

--- Proj_03/src/App.jsx
+++ Proj_03/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import PostList from './features/PostList'
+import PostList from './features/components/PostList'
+import ContentForm from './features/components/ContentForm'
+
 
 function App() {
 
   return (
     <>
       {/* <h1 className='bg-red-500 text-3xl'>Hello</h1> */}
+      <ContentForm />
       <PostList />
     </>
   )
