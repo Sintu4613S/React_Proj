68119c8c4cd290bc82a66dfa30301882ef5cc4a9
Sintu4613S <shiva49767@gmail.com>
2026-07-21 20:47:00 +0530

    enhance ContentForm to include author selection and update post dispatch logic

--- Proj_03/src/features/components/ContentForm.jsx
+++ Proj_03/src/features/components/ContentForm.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { postAdded } from '../post/postSlice'
-
+import { selectAllUsers } from '../users/usersSlice'
+import { useSelector } from 'react-redux'
 
 const ContentForm = () => {
   const dispatch = useDispatch()
+
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
 
-  const onTitleChange = (e) => setTitle(e.target.value)
-  const onContentChange = (e) => setContent(e.target.value)
+  const users = useSelector(selectAllUsers)
+
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content))
+      dispatch(postAdded(title, content, userId))
       setTitle('')
       setContent('')
     }
   }
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOption = users.map((user) => (
+    <option key={user.id} value={user.id} >
+      {user.name}
+    </option>
+  ))
+  // console.log('userid', usersOption)
   return (
     <>
       <div className="hero bg-base-200 h-full">
@@ -28,7 +42,7 @@ const ContentForm = () => {
                 <label
                   htmlFor='title'
                   className="label text-2xl
-                 text-shadow-white">Title
+                 text-shadow-white">Title:
                 </label>
                 <input
                   className='border-2 p-2 rounded-lg'
@@ -37,9 +51,23 @@ const ContentForm = () => {
                   value={title}
                   name='title'
                   placeholder="Enter title..."
-                  onChange={onTitleChange}
+                  onChange={onTitleChanged}
 
                 />
+                <label
+                  htmlFor='title'
+                  className="label text-2xl
+                 text-shadow-white">Author:
+                </label>
+                <select
+                  className='border-2 p-2 rounded-lg'
+                  id='auth'
+                  value={userId}
+                  onChange={onAuthorChanged}
+                >
+                  <option value=""></option>
+                  {usersOption}
+                </select>
                 <label
                   htmlFor='content'
                   className="label text-2xl text-shadow-white">Content
@@ -51,12 +79,13 @@ const ContentForm = () => {
                   value={content}
                   name='content'
                   placeholder="Enter Content here..."
-                  onChange={onContentChange}
+                  onChange={onContentChanged}
 
                 />
                 <button
                   className="btn btn-neutral mt-4"
                   onClick={onSavePostClicked}
+                  disabled={!canSave}
                 >Save Post</button>
               </fieldset>
             </div>
