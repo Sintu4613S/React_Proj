478aed44888b0725bc2f41841394678aac57245d
Sintu4613S <shiva49767@gmail.com>
2026-07-21 10:43:43 +0530

    refactor postSlice to use 'title' instead of 'name' and implement postAdded reducer

--- Proj_03/src/features/post/postSlice.js
+++ Proj_03/src/features/post/postSlice.js
@@ -1,18 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = [
-  { id: 1, name: "Introduction", content: "Welcome to learningJavaScript basics!" },
+  { id: 1, title: "Introduction", content: "Welcome to learningJavaScript basics!" },
   {
-    id: 2, name: "Variables", content: "Objects are variables that can contain many values."
+    id: 2, title: "Variables", content: "Objects are variables that can contain many values."
   }
-
 ]
 
 export const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {
+    // postAdded(state,action){
+    //   state.push(action.payload)
+    // }
+    postAdded:
+    {
+      reducer(state, action) {
+        state.unshift(action.payload)
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          }
+        }
+      }
+    }
 
   }
 })
+export const selectAllPosts = (state) => state.posts
+export const { postAdded } = postSlice.actions
+
 export default postSlice.reducer
