5fec540f5c77b36b3ae56200d8af9d5ea544868e
Sintu4613S <shiva49767@gmail.com>
2026-07-23 12:50:55 +0530

    remove the code

--- Proj_04/src/app/store.js
+++ Proj_04/src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import popReducer from '../features/popup/popSlice'
+//import deletReducer from '../features/popup/popSlice'
 export const store = configureStore({
   reducer: {
     popup: popReducer,
+    // delete: deletReducer,
   }
 })
