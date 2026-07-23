ee1ada8994fba0a4ba4f6e992c939fc8fb73d3ae
Sintu4613S <shiva49767@gmail.com>
2026-07-23 11:31:28 +0530

    change the z value

--- Proj_04/src/components/navbar/NavBar.jsx
+++ Proj_04/src/components/navbar/NavBar.jsx
@@ -1,16 +1,17 @@
 import Layout from "../layout/Layout"
 
 const NavBar = () => {
-
   return (
-    <div className="bg-base-100 sticky top-0 z-20">
+    <div className="bg-base-100 sticky top-0 z-10">
       <Layout>
         <div className="navbar bg-base-100 shadow-sm">
           <div className="flex-1">
             <a className="btn btn-ghost text-xl">Employees</a>
           </div>
           <div className="flex gap-4 ">
-            <button className="btn btn-square btn-ghost" >
+            <button
+              onClick={() => dispatch(openEmployePopup())}
+              className="btn btn-square btn-ghost" >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
               </svg>
