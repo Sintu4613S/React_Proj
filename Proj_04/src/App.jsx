91224eab9cbfabbafef0b94416aaa2bd990070b0
Sintu4613S <shiva49767@gmail.com>
2026-07-23 11:27:20 +0530

    import popup page

--- Proj_04/src/App.jsx
+++ Proj_04/src/App.jsx
@@ -3,13 +3,14 @@ import Employees from './components/employees/Employees'
 import Footer from './components/footer/Footer'
 import Layout from './components/layout/Layout'
 import NavBar from './components/navbar/NavBar'
-// import Footer from './components/footer/Footer'
-// import Navbar from './components/navbar/Navbar'
+import PopupPage from './features/page/PopupPage'
+
 
 function App() {
   return (
     // <Layout>
     <div className='w-full min-h-screen flex flex-col'>
+      <PopupPage />
       <NavBar />
       <div className='flex-1 py-10'>
         <Employees />
