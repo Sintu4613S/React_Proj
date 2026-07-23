9ebfad9211605e82dba8dc02b8050387c4628573
Sintu4613S <shiva49767@gmail.com>
2026-07-23 12:10:25 +0530

    refactor the import

--- Proj_04/src/App.jsx
+++ Proj_04/src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css'
-import Employees from './components/employees/Employees'
+
 import Footer from './components/footer/Footer'
 import Layout from './components/layout/Layout'
 import NavBar from './components/navbar/NavBar'
-import PopupPage from './features/page/PopupPage'
+import DeleteDetails from './features/delete/DeleteDetails'
+import Employees from './features/employees/Employees'
+import PopupPage from './features/popup/PopupPage'
+
 
 
 function App() {
   return (
     // <Layout>
     <div className='w-full min-h-screen flex flex-col'>
+      <DeleteDetails />
       <PopupPage />
       <NavBar />
       <div className='flex-1 py-10'>
         <Employees />
+
+
       </div>
       <Footer />
     </div>
