5a08e821aefe7707d67d665de875f682ef99f8e5
Sintu4613S <shiva49767@gmail.com>
2026-07-23 12:56:14 +0530

    first add a card inside the return  after that select the deletepopup state and make logic ,and  hide it if click outside the deletepopup div

---
+++ Proj_04/src/features/delete/DeleteDetails.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { closeDeletepopup } from '../popup/popSlice'
+
+const DeleteDetails = () => {
+  const dispatch = useDispatch()
+  const showDeletePopup = useSelector((state) => state.popup?.deletepopup)
+  console.log("Delete Popup Value hai", showDeletePopup)
+  if (!showDeletePopup) {
+    return null
+  }
+  return (
+    <div onClick={() => dispatch(closeDeletepopup())} className=' fixed top-0 w-full h-full bg-black/80 flex items-center justify-center z-10'>
+      <div onClick={(e) => e.stopPropagation()} className="card bg-base-100  w-96 shadow-sm ">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Delete!</h2>
+          <p>Are You Sure Want to delete?</p>
+          <div className="card-actions">
+            <button className="btn btn-primary">No</button>
+            <button className="btn btn-error">yes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default DeleteDetails
