89782be9b8b8a95d43103fb2351a19186d2a5cb5
Sintu4613S <shiva49767@gmail.com>
2026-07-23 12:52:29 +0530

    Add two more reducer for deletepopup and export it

--- Proj_04/src/features/popup/popSlice.js
+++ Proj_04/src/features/popup/popSlice.js
@@ -19,11 +19,19 @@ const popupSlice = createSlice({
     closeEmployepopup(state) {
       state.employepopup = false
 
+    },
+    openDeletePopup(state, action) {
+      state.deletepopup = action.payload ?? true
+
+    },
+    closeDeletepopup(state) {
+      state.deletepopup = false
     }
+
   },
 })
 
 // export const allPopUps = state => state.pops
-export const { openEmployePopup, closeEmployepopup } = popupSlice.actions
+export const { openEmployePopup, closeEmployepopup, openDeletePopup, closeDeletepopup } = popupSlice.actions
 //export const selectAllPopup = (state) => state.popup
 export default popupSlice.reducer
