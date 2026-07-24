afb339d7ca5ae3648835e41e9079d8ed0e1852a7
Sintu4613S <shiva49767@gmail.com>
2026-07-24 16:23:33 +0530

    import the useEfeect for api call,Empeditform pageand useDispatch

--- Proj_04/src/App.jsx
+++ Proj_04/src/App.jsx
@@ -1,23 +1,34 @@
+import { useEffect } from 'react'
 import './App.css'
 
 import Footer from './components/footer/Footer'
 import Layout from './components/layout/Layout'
 import NavBar from './components/navbar/NavBar'
 import DeleteDetails from './features/delete/DeleteDetails'
+import EmpeditForm from './features/edit/EmpeditForm'
 import Employees from './features/employees/Employees'
 import PopupPage from './features/popup/PopupPage'
+import { getEmployees } from './features/employees/employeethunk'
+import { useDispatch } from 'react-redux'
 
 
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getEmployees())
+
+  })
   return (
     // <Layout>
     <div className='w-full min-h-screen flex flex-col'>
-      <DeleteDetails />
-      <PopupPage />
+
       <NavBar />
       <div className='flex-1 py-10'>
         <Employees />
+        <PopupPage />
+        <DeleteDetails />
+        <EmpeditForm />
 
 
       </div>
