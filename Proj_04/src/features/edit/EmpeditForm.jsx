7e2f8e4702bc2a20a8b533fe0fd0d62652f8c0ef
Sintu4613S <shiva49767@gmail.com>
2026-07-24 18:13:13 +0530

    change the tailwind css

--- Proj_04/src/features/edit/EmpeditForm.jsx
+++ Proj_04/src/features/edit/EmpeditForm.jsx
@@ -9,7 +9,7 @@ const EmpeditForm = () => {
   console.log('Edittab render hua', showEditpopup)
   if (!showEditpopup) return null;
   return (
-    <div onClick={() => dispatch(closeEdittab())} className=' fixed top-4 w-full h-full bg-black/80 flex items-center justify-center z-20'>
+    <div onClick={() => dispatch(closeEdittab())} className=' fixed top-0 w-full h-full bg-black/80 flex items-center justify-center z-20'>
       <form onClick={(e) => e.stopPropagation()} className=" max-w-sm mx-auto border w-sm p-4 rounded-lg">
 
         <label htmlFor="url" className="block  text-lg font-medium text-heading">InputUrl</label>
@@ -26,7 +26,7 @@ const EmpeditForm = () => {
 
 
         <label htmlFor="message" className="block mt-1 text-lg font-medium text-heading">Message</label>
-        <textarea id="message" rows="4" className=" mb-1.5 bg-base-100 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body" placeholder="Write your thoughts here..."></textarea>
+        <textarea id="message" rows="4" className=" mb-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body" placeholder="Write your thoughts here..."></textarea>
 
         <div className='flex gap-4 justify-end items-end'>
           <button className='btn'>Cancel</button>
